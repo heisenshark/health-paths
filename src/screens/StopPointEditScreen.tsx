2b76307dd21fd523947c403ce3355fb2ad69a474
sylay1 <sylaypaper@gmail.com>
2022-12-03 03:11:15 +0100

    added option to play audio

--- src/screens/StopPointEditScreen.tsx
+++ src/screens/StopPointEditScreen.tsx
@@ -1,16 +1,19 @@
 import * as React from "react";
-import { Text, View, StyleSheet, ScrollView, Image } from "react-native";
+import { Text, View, StyleSheet, ScrollView, Image, Button } from "react-native";
 import SquareButton from "./../components/SquareButton";
 import { Icon } from "react-native-vector-icons/FontAwesome";
 import tw from "../lib/tailwind";
 import Waypoint from "./../utils/interfaces";
 import { TextInput } from "react-native-paper";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
-
-interface StopPointEditScreenProps {
-  editedWaypoint: Waypoint;
-}
+import DocumentPicker, {
+    DirectoryPickerResponse,
+    DocumentPickerResponse,
+    isInProgress,
+    types,
+} from "react-native-document-picker";
+import { Audio } from "expo-av";
 
 const StopPointEditScreen = ({ navigation, route }) => {
     const { editedWaypoint } = route.params as { editedWaypoint: Waypoint };
@@ -19,10 +22,51 @@ const StopPointEditScreen = ({ navigation, route }) => {
 
     const [image, setImage] = useState(null);
 
+    const [result, setResult] = useState<
+    Array<DocumentPickerResponse> | DirectoryPickerResponse | undefined | null
+  >(null);
+
+    const [sound, setSound] = React.useState();
+
+    async function playSound() {
+        console.log("Loading Sound");
+        console.log(result, result.uri);
+
+        const { sound } = await Audio.Sound.createAsync({ uri: result.uri });
+        setSound(sound);
+
+        console.log("Playing Sound");
+        await sound.playAsync();
+    }
+
+    React.useEffect(() => {
+        return sound
+            ? () => {
+                console.log("Unloading Sound");
+                sound.unloadAsync();
+            }
+            : undefined;
+    }, [sound]);
+
+    useEffect(() => {
+        console.log(JSON.stringify(result, null, 2));
+    }, [result]);
+
+    const handleError = (err: unknown) => {
+        if (DocumentPicker.isCancel(err)) {
+            console.warn("cancelled");
+            // User cancelled the picker, exit any dialogs or menus and move on
+        } else if (isInProgress(err)) {
+            console.warn("multiple pickers were opened, only the last will be considered");
+        } else {
+            throw err;
+        }
+    };
+
     const pickImage = async () => {
     // No permissions request is necessary for launching the image library
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
@@ -32,9 +76,8 @@ const StopPointEditScreen = ({ navigation, route }) => {
 
         if (!result.canceled) {
             setImage(result.assets[0].uri);
+            editedWaypoint.image = result.assets[0].uri;
         }
-
-        editedWaypoint.image = result.assets[0].uri;
     };
 
     return (
@@ -44,7 +87,7 @@ const StopPointEditScreen = ({ navigation, route }) => {
                 <View>
                     <Image
                         style={tw`aspect-square w-full h-auto justify-center self-center my-4 border-4 border-black rounded-2xl`}
-                        source={{ uri: editedWaypoint.image }}
+                        source={{ uri: image }}
                     />
                     <SquareButton
                         addStyle={"absolute bottom-8 right-4"}
@@ -64,13 +107,27 @@ const StopPointEditScreen = ({ navigation, route }) => {
                         <SquareButton
                             addStyle={"ml-auto"}
                             label={"Odtwórz"}
-                            onPress={() => {
-                                console.log("Otwieram audio");
-                            }}></SquareButton>
+                            onPress={() => playSound()}></SquareButton>
                     </View>
+                    <Button
+                        title="open picker for single file selection"
+                        onPress={async () => {
+                            try {
+                                const pickerResult = await DocumentPicker.pickSingle({
+                                    presentationStyle: "fullScreen",
+                                    copyTo: "cachesDirectory",
+                                    type: [types.audio],
+                                }).then((res) => {
+                                    setResult(res);
+                                });
+                            } catch (e) {
+                                handleError(e);
+                            }
+                        }}
+                    />
                 </View>
                 <TextInput
-                    style={tw`rounded-xl bg-main-1 text-2xl`}
+                    style={tw`bg-white text-2xl border-4 border-secondary-1 `}
                     placeholder="Nazwa"
                     value={name}
                     onChangeText={(text) => {
@@ -78,7 +135,7 @@ const StopPointEditScreen = ({ navigation, route }) => {
                     }}
                     label="Nazwa Punktu"></TextInput>
                 <TextInput
-                    style={tw`h-60 rounded-xl my-4 bg-main-1 text-2xl`}
+                    style={tw`h-60 text-2xl rounded-xl my-4 bg-white border-secondary-1 border-4`}
                     placeholder="Opis"
                     value={description}
                     onChangeText={(text) => {
